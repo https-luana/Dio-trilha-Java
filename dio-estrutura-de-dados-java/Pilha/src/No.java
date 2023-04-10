b0c33af3fbfe0f156b845bae64da3994dadbdf10
https-luana <zusumi621@gmail.com>
2023-04-10 15:45:13 -0300

    Implementação de Metodo Top,Push e Pop

---
+++ dio-estrutura-de-dados-java/Pilha/src/No.java
@@ -0,0 +1,40 @@
+public class No {
+        private int dado;
+        private No refNo = null;
+
+        public No(){
+            
+        }
+
+        public No(int dado) {
+            this.dado = dado;
+        }
+
+        public int getDado() {
+            return dado;
+        }
+
+        public void setDado(int dado) {
+            this.dado = dado;
+        }
+
+        public No getReNo() {
+            return refNo;
+        }
+
+        public void setRefNo(No reNo) {
+            this.refNo = reNo;
+        }
+
+        @Override
+        public String toString() {
+            return "No [dado=" + dado + "]";
+        }
+
+        public No getRefNo() {
+            return null;
+        }
+
+        
+    }
+
