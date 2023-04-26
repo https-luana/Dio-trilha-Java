552434b6fd5b8ba1d4b5404f51343e186971175f
https-luana <zusumi621@gmail.com>
2023-04-25 22:06:05 -0300

    Listas Encadeadas

---
+++ dio-estrutura-de-dados-java/ListaCircular/ListaCircular/src/No.java
@@ -0,0 +1,34 @@
+public class No<T> {
+
+    private T conteudo;
+    private No<T> noproximo;
+   
+    public No(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No<T> getNoproximo() {
+        return noproximo;
+    }
+
+    public void setNoproximo(No<T> noproximo) {
+        this.noproximo = noproximo;
+    }
+
+    @Override
+    public String toString() {
+        return "No [conteudo=" + conteudo + "]";
+    }
+
+    
+    
+    }
+
